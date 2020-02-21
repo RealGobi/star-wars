ede076438c01b777cfc087af335898239afd68d8
Jimmy Svensson <jimmy888swe@gmail.com>
2020-02-22 00:00:46 +0100

    feat: added isloading check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [isloading, setloading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const loading = (<div>Loading...</div>)
+  const displayMovies = (<div>movies...</div>)
+
   return (
     <div className="App">
+    {isloading ? loading : displayMovies}
     </div>
   );
 }
