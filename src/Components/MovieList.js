73434d75d46ab7f5a4af21dbfdbeabbf54463851
Jimmy Svensson <jimmy888swe@gmail.com>
2020-02-22 16:02:22 +0100

    feat: sort func to get order by release

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Movie from './Movie/Movie';
+import './Movie-list.css';
 
 export default function MovieList( { movies } ) {
+
   return (
-    <>
-    { movies.map((movie, idx) => (
-      <Movie 
-        key = {idx}
-        movie = {movie} 
-        />
-    ))}
-    </>
+    <div className="center">
+      <div className="grid-container">
+      { movies.sort((a, b) => (a.release_date > b.release_date) ? 1 : -1).map((movie, idx) => (
+        <Movie
+          key = {idx}
+          movie = {movie}
+          />
+      ))}
+      </div>
+    </div>
   )
 }
