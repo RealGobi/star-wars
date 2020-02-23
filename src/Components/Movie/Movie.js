86364a9a52437adadb1c09c27c5bb39b86b36fff
Jimmy Svensson <jimmy888swe@gmail.com>
2020-02-23 22:22:18 +0100

    feat: all info for character

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -4,9 +4,6 @@ import './Movie.css';
 
 export default function Movie({ movie }) {
 
-  // state with loading to true as initial state
-  const [characters, setCharacters] = useState([]);
-
   // trigger inside model, that renders first
   const clickOnCard = () => (
     <div className="single-movie" onClick={() => Modal()}>
@@ -17,34 +14,34 @@ export default function Movie({ movie }) {
 
   // when clicked, content inside Popup renders
   const Modal = () => (
-    <Popup
-      trigger={clickOnCard()}
-      modal
-      closeOnDocumentClick
-    >
-      {close => (
-        <div className="popup-content">
-          <div className="pop-up-grid">
-            <div className="title-in-pop-up">{ movie.title }</div>
-            <span id="close" onClick={close}>Close</span>
-            <div className="character">Characters</div>
-              <div className="column">
-                { movie.characters.map((characterUrl) => {
-                  const fetchData = async () => {
-                    const response = await fetch(characterUrl); 
-                    const data = await response.json();
-                      setCharacters(data);
+  <Popup
+    trigger={clickOnCard()}
+    modal
+    closeOnDocumentClick
+  >
+    {close => (
+      <div className="popup-content">
+        <div className="pop-up-grid">
+          <div className="title-in-pop-up">{ movie.title }</div>
+          <span id="close" onClick={close}>Close</span>
+          <div className="character">Characters</div>
+            <div className="column">
+              { movie.characters.map(characterUrl => {
+                const fetchData = async () => {
+                  const response = await fetch(characterUrl);
+                  const data = await response.json();
+                  console.log(data.name);
+                  return <span>{data.name}</span>;
                   }
-                  fetchData();
-                })}
+                fetchData();
+              })}
 
-              </div>
-          </div>
+            </div>
         </div>
-      )}
-    </Popup>
+      </div>
+    )}
+  </Popup>
   );
-  console.log(characters.name);
 
   return (
     <Modal />
