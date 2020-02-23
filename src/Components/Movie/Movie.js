09c5ca3fc07785bdb8dbbb9e7aad95e0b4186096
Jimmy Svensson <jimmy888swe@gmail.com>
2020-02-23 01:55:11 +0100

    feat: popup and layout

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import Popup from "reactjs-popup";
 import './Movie.css';
 
 export default function Movie( { movie }) {
-  return (
-    <div className="card">
-      <div className="single-movie">
-        <span id="title">{ movie.title }</span>
-        <span id="release">{ movie.release_date }</span>
-      </div>
+console.log(movie);
+
+  // trigger inside model, that renders first
+  const clickOnCard = () => (
+    <div className="single-movie" onClick={() => Modal()}>
+      <span id="title">{ movie.title }</span>
+      <span id="release">{ movie.release_date }</span>
     </div>
+  );
+
+  // when clicked, content inside Popup renders
+  const Modal = () => (
+    <Popup
+      trigger={clickOnCard()}
+      modal
+      closeOnDocumentClick
+    >
+      {close => (
+        <div className="popup-content">
+          <div className="pop-up-grid">
+            <div className="title-in-pop-up">{ movie.title }</div>
+            <p id="close" onClick={close}>Close</p>
+          </div>
+        </div>
+      )}
+    </Popup>
+  );
+
+
+  return (
+    <Modal />
   )
 }
