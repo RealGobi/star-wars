32251d2ccbbd7573852deddb9779dfc1b0ff0e60
Jimmy Svensson <jimmy888swe@gmail.com>
2020-02-24 22:34:51 +0100

    fix: removed reactjs popup

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,49 +1,22 @@
-import React, {useState} from 'react';
-import Popup from "reactjs-popup";
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import './Movie.css';
 
 export default function Movie({ movie }) {
 
-  // trigger inside model, that renders first
-  const clickOnCard = () => (
-    <div className="single-movie" onClick={() => Modal()}>
-      <span id="title">{ movie.title }</span>
-      <span id="release">{ movie.release_date }</span>
-    </div>
-  );
-
-  // when clicked, content inside Popup renders
-  const Modal = () => (
-  <Popup
-    trigger={clickOnCard()}
-    modal
-    closeOnDocumentClick
-  >
-    {close => (
-      <div className="popup-content">
-        <div className="pop-up-grid">
-          <div className="title-in-pop-up">{ movie.title }</div>
-          <span id="close" onClick={close}>Close</span>
-          <div className="character">Characters</div>
-            <div className="column">
-              { movie.characters.map(characterUrl => {
-                const fetchData = async () => {
-                  const response = await fetch(characterUrl);
-                  const data = await response.json();
-                  console.log(data.name);
-                  return <span>{data.name}</span>;
-                  }
-                fetchData();
-              })}
-
-            </div>
-        </div>
-      </div>
-    )}
-  </Popup>
-  );
+  const Modal = () => {
+    
+  }
 
   return (
-    <Modal />
+      <div className="single-movie" onClick={() => Modal()}>
+        <span id="title">{ movie.title }</span>
+        <span id="release">{ movie.release_date }</span>
+      </div>
   )
+}
+
+Movie.propTypes = {
+  movie: PropTypes.object.isRequired
 }
