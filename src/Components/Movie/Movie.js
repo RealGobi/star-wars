8d703a6075f4300ee5a3bea86952f3406b277aa8
Jimmy Svensson <jimmy888swe@gmail.com>
2020-02-25 09:18:28 +0100

    feat: fetch in modal and pass res to movie

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Toggle from '../Popup/Toggle';
 import Modal from '../Popup/Modal';
 
 import './Movie.css';
 export default function Movie({ movie }) {
+  
+  const [character, setCharacter] = useState([])
+console.log(character);
 
   return (
     <>
@@ -15,15 +18,17 @@ export default function Movie({ movie }) {
           <span id="release">{ movie.release_date }</span>
         </div>}
           content={hide => (
-            <Modal className="modal">
+            <Modal movie={movie} setCharacter={setCharacter} className="modal">
             <div className="popup-content">
             <div className="pop-up-grid">
               <div className="title-in-pop-up">{ movie.title }</div>
               <span id="close" onClick={hide}>Close</span>
               <div className="character">Characters</div>
-                <div className="column">
-                  
-                </div>
+              <div className="column">
+              <ul>
+               {character.map((char, idx) => <li key={idx}>{char.name}</li>)}
+              </ul>       
+          </div>
             </div>
           </div>
             </Modal>
--- src/Components/Popup/Modal.js
+++ src/Components/Popup/Modal.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import './Modal.css';
 
-  const Modal = ({ children }) => (
+  const Modal = ({ children, movie, setCharacter }) => {
+
+      useEffect(() => {  
+        const arr = []; 
+        movie.characters.map(char => {
+          const fetchData = async () => {
+            const response = await fetch(char); 
+            const data = await response.json();
+            arr.push(data)
+            setCharacter(arr);
+          }
+          fetchData();
+        })
+      }, [])
+    
+    return(
     ReactDOM.createPortal(
       <div className="modal">
         {children}
       </div>,
       document.getElementById('modal-root')
     )
-  );
+    )};
 
 export default Modal;
