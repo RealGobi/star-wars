432b2635c1483a75cbc73f4309dc203b93372e7d
Jimmy Svensson <jimmy888swe@gmail.com>
2020-02-25 01:08:31 +0100

    fix: update movie comp with modal and toggle

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Toggle from '../Popup/Toggle';
+import Modal from '../Popup/Modal';
 
 import './Movie.css';
-
 export default function Movie({ movie }) {
 
-  const Modal = () => {
-    
-  }
-
   return (
-      <div className="single-movie" onClick={() => Modal()}>
-        <span id="title">{ movie.title }</span>
-        <span id="release">{ movie.release_date }</span>
-      </div>
+    <>
+      <Toggle
+        toggle={show => 
+        <div className="single-movie" onClick={show}>
+          <span id="title">{ movie.title }</span>
+          <span id="release">{ movie.release_date }</span>
+        </div>}
+          content={hide => (
+            <Modal className="modal">
+            <div className="popup-content">
+            <div className="pop-up-grid">
+              <div className="title-in-pop-up">{ movie.title }</div>
+              <span id="close" onClick={hide}>Close</span>
+              <div className="character">Characters</div>
+                <div className="column">
+                  
+                </div>
+            </div>
+          </div>
+            </Modal>
+          )}
+        />
+     
+      </>
   )
 }
 
